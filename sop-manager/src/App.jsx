67227b333aba64e67e4790c5471d68e0fcbec015
porchsal <porchsal@yahoo.com>
2024-12-03 14:25:18 -0500

    Initial Setup

---
+++ sop-manager/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Chief Medical Supplies
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ sop-manager/src/pages/sop_catalog.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function sop_catalog() {
+  return (
+    <div>sop_catalog</div>
+  )
+}
